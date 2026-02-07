639a4fcfbc089bab7c9d04b029af31e675749e72
leolu0602 <lyuholu@gmail.com>
2026-02-07 16:30:21 -0500

    feat: write test

--- scheduling-xv6-lottery/xv6/test.c
+++ scheduling-xv6-lottery/xv6/test.c
@@ -1,7 +1,43 @@
 #include "types.h"
+#include "stat.h"
 #include "user.h"
 
-int main(int argc, char *argv[]) {
-    printf(1, "settickets: %d\n", settickets(69));
+int main(void) {
+    int pid;
+
+    // child A: 30 tickets
+    pid = fork();
+
+    if (pid == 0) {
+        settickets(30);
+        
+        for(int i = 0; i < 100000000; ++i)
+            ; 
+    }
+
+    // child B: 20 tickets
+    pid = fork();
+
+    if (pid == 0) {
+        settickets(20);
+        
+        for(int i = 0; i < 100000000; ++i)
+            ; 
+    }
+
+    // child C: 10 tickets
+    pid = fork();
+
+    if (pid == 0) {
+        settickets(10);
+        
+        for(int i = 0; i < 100000000; ++i)
+            ; 
+    }
+
+    // parent just waits
+    while (wait() > 0)
+        ;
+
     exit();
 }
