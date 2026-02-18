7f8c37537e6e2bec91df54310d45c7073341d6e7
leolu0602 <lyuholu@gmail.com>
2026-02-17 22:58:28 -0500

    feat: pass arg to workers

--- concurrency-pzip/pzip.c
+++ concurrency-pzip/pzip.c
@@ -2,10 +2,36 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 #include <sys/stat.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef struct {
+  char *data;
+  int start;
+  int end;
+} thread_arg;
+
+void *worker(void *arg) {
+  thread_arg *p = (thread_arg *)arg;
+
+  printf("%d, %d\n", p->start, p->end);
+  free(p);
+
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("pzip: file1 [file2 ...]\n");
+    exit(1);
+  }
+
+  int fd;
+
+  if ((fd = open(argv[1], O_RDONLY)) == -1) {
+    printf("pzip: open failed\n");
     exit(1);
   }
   
@@ -16,46 +42,40 @@ int main(int argc, char *argv[]) {
     printf("pzip: stat failed\n");
     exit(1);
   }
-
-  int chunk_size = statbuf.st_size / n;
   
-  printf("chunk_size: %d\n", chunk_size);
-  /*
-  FILE *fp;
-  char c;
-  char last = EOF;
-  int cnt = 0;
+  char *data;
 
-  for (int i = 1; i < argc; ++i) {
-    if (!(fp = fopen(argv[i], "r"))) {
-      printf("pzip: cannot open file\n");
-      exit(1);
-    }
-    
-    while ((c = fgetc(fp)) != EOF) {
-      if (c == last) {
-	++cnt;
-	continue;
-      }
-      
-      if (last != EOF) {
-	fwrite(&cnt, sizeof(cnt), 1, stdout);
-	fwrite(&last, sizeof(last), 1, stdout);
-      }
-     
-      last = c;
-      cnt = 1;
-    }
+  if ((data = mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
+    printf("pzip: mmap failed\n");
+    close(fd);
+    exit(1);
+  }
+  
+  int chunk_size = statbuf.st_size / n;
+  pthread_t threads[n];
 
-    if (fclose(fp)) {
-      printf("pzip: cannot close file\n");
+  for (int i = 0; i < n; ++i) {
+    thread_arg *p = (thread_arg *)malloc(sizeof(thread_arg));
+
+    p->data = data;
+    p->start = i * chunk_size;
+    p->end = (i + 1) * chunk_size - 1;
+
+    if (pthread_create(&threads[i], NULL, worker, p) != 0) {
+      printf("pzip: pthread_create failed\n");
       exit(1);
     }
   }
-
-  fwrite(&cnt, sizeof(cnt), 1, stdout);
-  fwrite(&last, sizeof(last), 1, stdout);
-  */
+  
+  close(fd);
+  munmap(data, statbuf.st_size);
+  
+  for (int i = 0; i < n; ++i) {
+    if (pthread_join(threads[i], NULL) != 0) {
+      printf("pzip: pthread_join failed\n");
+      exit(1);
+    }
+  }
 
   return 0;
 }
